a80526e00d5f1cc71f26d058dd5cae8f99bb7f4f
shakthiwork1999 <shakthiwork1999@gmail.com>
2026-02-19 22:41:28 +0530

    Third commit

--- TestJavaFile.java
+++ TestJavaFile.java
@@ -2,5 +2,7 @@ public class TestJavaFile{
 	public static void main(String args[]){
 		System.out.println("Hello world");
 		System.out.println("Starting off with Jenkins integration with Git using java project");
+		System.out.println(10+20);
+
 	}
 }
